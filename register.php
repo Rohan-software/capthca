b7bd93f92cecef39b4f930d1145e39fc2813fb81
Rohan-software <h765029@gmail.com>
2025-04-13 15:20:20 +0530

    register.php

---
+++ register.php
@@ -0,0 +1,22 @@
+<?php include("includes/config.php"); ?>
+<form method="POST" action="">
+    <input type="text" name="username" placeholder="Username" required>
+    <input type="email" name="email" placeholder="Email" required>
+    <input type="password" name="password" placeholder="Password" required>
+    <button type="submit" name="register">Register</button>
+</form>
+
+<?php
+if (isset($_POST['register'])) {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+    $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+    if ($conn->query($sql) === TRUE) {
+        echo "Registration Successful!";
+    } else {
+        echo "Error: " . $conn->error;
+    }
+}
+?>
